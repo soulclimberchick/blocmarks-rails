e7b8290a4248794a9ac6bb7526c93f061770b976
Kira Helm <soulclimberchick@gmail.com>
2019-09-23 19:23:17 -0700

    fixed user confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable,
+         :recoverable, :rememberable, :trackable, :confirmable,
          :validatable, :authentication_keys => {email: true, login: false}
 
-  validates :username, presence: :true, uniqueness: { case_sensitive: false }       
+  validates :username, presence: :true, uniqueness: { case_sensitive: false }
   validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
   validate :validate_username
 
